5e326a64485b4cd52ab93fb2daac2bfe1adc5674
Kelvin Julian Putra <julianputrakelvin@gmail.com>
2025-10-08 19:36:20 +0700

    docs: add supplementary comments in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'pages/register_page.dart';
+// import 'pages/home_page.dart';
+
 // import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
